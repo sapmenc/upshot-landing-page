f4c072efd8850bcd074621bd30c96b4363b4f53a
Atharv Kishor <Atharvkishoratharv@gmail.com>
2024-11-23 20:17:11 +0530

    fixed email box

--- src/components/ui/placeholders-and-vanish-input.tsx
+++ src/components/ui/placeholders-and-vanish-input.tsx
@@ -96,7 +96,8 @@ export function PlaceholdersAndVanishInput({
       x,
       y,
       r: 1,
-      color: `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`,
+      // color: `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`,
+      color: `rgba(${color[255]}, ${color[255]}, ${color[255]}, ${color[255]})`,
     }));
   }, [value]);
 
@@ -176,12 +177,13 @@ export function PlaceholdersAndVanishInput({
   };
   return (
     <form
-      className={cn(
-        "w-full relative max-w-xl mx-auto bg-[#1B1B1B] dark:bg-zinc-800 h-12 rounded-full overflow-hidden shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200",
-        value && "bg-gray-50"
-      )}
-      onSubmit={handleSubmit}
-    >
+  className={cn(
+    // "w-full relative max-w-xl mx-auto bg-[#1B1B1B] dark:bg-zinc-800 h-12 rounded-full overflow-hidden shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200"
+    "w-full relative max-w-xl mx-auto bg-[#1B1B1B] dark:bg-zinc-800 h-12 rounded-full overflow-hidden shadow-[0px_2px_3px_-1px_rgba(255,255,255,0.1),_0px_1px_0px_0px_rgba(255,255,255,0.02),_0px_0px_0px_1px_rgba(255,255,255,0.08)] transition duration-200"
+  )}
+  onSubmit={handleSubmit}
+>
+
       <canvas
         className={cn(
           "absolute pointer-events-none  text-base transform scale-50 top-[20%] left-2 sm:left-8 origin-top-left filter invert dark:invert-0 pr-20",
@@ -201,7 +203,7 @@ export function PlaceholdersAndVanishInput({
         value={value}
         type="text"
         className={cn(
-          "w-full relative text-sm sm:text-base z-50 border-none dark:text-white bg-transparent text-black h-full rounded-full focus:outline-none focus:ring-0 pl-4 sm:pl-10 pr-20",
+          "w-full relative text-sm sm:text-base z-50 border-none dark:text-[#9E9E9E] bg-transparent  text-[#9E9E9E] h-full rounded-full focus:outline-none focus:ring-0 pl-4 sm:pl-10 pr-20",
           animating && "text-transparent dark:text-transparent"
         )}
       />
@@ -209,7 +211,7 @@ export function PlaceholdersAndVanishInput({
       <button
         disabled={!value}
         type="submit"
-        className="absolute right-2 top-1/2 z-50 -translate-y-1/2 h-8 w-8 rounded-full bg-[#1B1B1B] dark:bg-zinc-900 dark:disabled:bg-zinc-800 transition duration-200 flex items-center justify-center"
+        className="absolute right-2 top-1/2 z-50 -translate-y-1/2 h-8 w-8 rounded-full disabled:bg-[#1b1b1b] bg-black dark:bg-zinc-900 dark:disabled:bg-zinc-800 transition duration-200 flex items-center justify-center"
       >
         <motion.svg
           xmlns="http://www.w3.org/2000/svg"
