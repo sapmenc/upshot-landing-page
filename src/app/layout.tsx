aaed1f783e55379ed2ba04a1d3f42fa3602db615
Atharv Kishor <Atharvkishoratharv@gmail.com>
2024-12-05 18:07:34 +0530

    ui changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,23 @@ export const metadata: Metadata = {
     icon: "/favicon.ico", // Path to your favicon
     shortcut: "/favicon.ico", // Optional, for older browsers
     apple: "/favicon.png", // Optional, for Apple devices
+    },
+  openGraph:{
+    title: "Coming To Power Your Team's Productivity | Upshot",
+    description: "Be among the first to unlock exclusive access to Upshot's groundbreaking features! Join our waitlist today and enjoy early bird perks designed to give you a seamless head start when we launch. Don’t miss out on this game-changing opportunity!",
+    countryName:"India",
+    ttl:60,
+    images:"/OG_Image.png"
+  },
+  verification:{
+    google:"google",
+    yandex:"yandex",
+    yahoo:"yahoo",
+    other:{
+      me:["product.sapmenc@gmail.com","https://upshot.co.in"],
     }
+  } 
+
 };
 
 export default function RootLayout({
